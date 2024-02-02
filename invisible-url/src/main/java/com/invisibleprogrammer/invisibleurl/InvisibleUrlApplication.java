dc4de9710f8b493e7cc6b44d0ba35bee516df5fb
Zsolt Miskolczi <zsolt.miskolczi@gmail.com>
2024-02-02 08:58:10 +0100

    Backend skeleton with JPA settings

---
+++ invisible-url/src/main/java/com/invisibleprogrammer/invisibleurl/InvisibleUrlApplication.java
@@ -0,0 +1,13 @@
+package com.invisibleprogrammer.invisibleurl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvisibleUrlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvisibleUrlApplication.class, args);
+	}
+
+}
