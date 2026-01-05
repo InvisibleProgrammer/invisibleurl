180615a7bb1be36b82d9e67ac49ccea1e7d9096a
InvisibleProgrammer <zsolt.miskolczi@gmail.com>
2026-01-05 15:10:28 +0100

    Open actuator health endpoint

---
+++ src/main/java/com/invisibleprogrammer/invisibleurl/SecurityConfiguration.java
@@ -0,0 +1,21 @@
+package com.invisibleprogrammer.invisibleurl;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfiguration {
+
+    @Bean
+    public SecurityFilterChain securityWebFilterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+                .authorizeHttpRequests(
+                    auth ->
+                        auth
+                                .requestMatchers("/actuator/**").permitAll()
+                                .anyRequest().authenticated()
+                ).build();
+    }
+}
